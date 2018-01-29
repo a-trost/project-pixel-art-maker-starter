ff360c8cc08d17ff03baa6393d6b3c39e011cea6
Alexander Trost <AlexRTrost@gmail.com>
2018-01-28 20:21:45 -0500

    Created Make Grid function

--- designs.js
+++ designs.js
@@ -1,10 +1,22 @@
 // Select color input
+let colorPicker = document.getElementById('colorPicker');
 // Select size input
+const submitButton = document.getElementById('submitButton');
+submitButton.addEventListener('click', makeGrid);
 
-// When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+    const pixelCanvas = document.getElementById('pixelCanvas');
+    while (pixelCanvas.firstChild) {
+    pixelCanvas.removeChild(pixelCanvas.firstChild);
 }
+    let height = document.getElementById("inputHeight").value;
+    let width = document.getElementById("inputWidth").value;
+    for (y=0; y < height; y++) {
+        let row = pixelCanvas.insertRow(0);
+        for (x=0; x < width; x++) {
+            var cell = row.insertCell(0);
+        }
+    }
+
+};
